c5eb43f5f743098226776c307ff5d2e5773fabce
Or Toledano <ortoledano@protonmail.com>
2020-11-20 09:59:27 +0200

    some updates

--- trainer.py
+++ trainer.py
@@ -112,17 +112,17 @@ class Trainer:
         return BCE + KLD
 
     def train(self, epoch):
-        model.train()
+        self.model.train()
         train_loss = 0
         for batch_idx, (data, _) in enumerate(self.data.train):
-            data = data.to(device)
-            optimizer.zero_grad()
-            recon_batch, mu, logvar = model(data)
-            loss = loss_function(recon_batch, data, mu, logvar)
+            data = data.to(self.device)
+            self.optimizer.zero_grad()
+            recon_batch, mu, logvar = self.model(data)
+            loss = self.loss_function(recon_batch, data, mu, logvar)
             loss.backward()
             train_loss += loss.item()
-            optimizer.step()
-            if batch_idx % args.log_interval == 0:
+            self.optimizer.step()
+            if batch_idx % self.args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                            100. * batch_idx / len(train_loader),
@@ -136,38 +136,38 @@ class Trainer:
         test_loss = 0
         with torch.no_grad():
             for i, (data, _) in enumerate(test_loader):
-                data = data.to(device)
-                recon_batch, mu, logvar = model(data)
-                test_loss += loss_function(recon_batch, data, mu, logvar).item()
+                data = data.to(self.device)
+                recon_batch, mu, logvar = self.model(data)
+                test_loss += self.loss_function(recon_batch, data, mu, logvar).item()
                 if i == 0:
                     n = min(data.size(0), 8)
                     comparison = torch.cat([data[:n],
-                                            recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                                            recon_batch.view(self.args.batch_size, 1, 28, 28)[:n]])
                     save_image(comparison.cpu(),
                                'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
-        test_loss /= len(test_loader.dataset)
+        test_loss /= len(self.test_loader.dataset)
         print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
 def main():
     parser = argparse.ArgumentParser(description='VAE MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                        help='input batch size for training (default: 128)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
+    parser.add_argument('--batch-size', type=int, default=128,
+                        help='batch size for training')
+    parser.add_argument('--epochs', type=int, default=10,
+                        help='epochs for training')
+    parser.add_argument('--cpu', action='store_true', default=False,
+                        help='force CPU usage')
+    parser.add_argument('--seed', type=int, default=42, metavar='S',
+                        help='random seed')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
     args = parser.parse_args()
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    args.cpu = args.cpu or not torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda" if args.cuda else "cpu")
+    device = torch.device("cpu" if args.cpu else "cuda")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
@@ -178,7 +178,7 @@ def main():
         trainer.test_epoch(epoch)
         with torch.no_grad():
             sample = torch.randn(64, 20).to(device)
-            sample = model.decode(sample).cpu()
+            sample = trainer.model.decode(sample).cpu()
             save_image(sample.view(64, 1, 28, 28),
                        'results/sample_' + str(epoch) + '.png')
 
