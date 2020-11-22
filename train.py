cc50b6a543158f09c7f1c3014500a921f164a8c5
yanirmr <yanirmr@gmail.com>
2020-11-22 15:05:01 +0200

    Add sequence of runs with different parameters.

--- train.py
+++ train.py
@@ -36,6 +36,7 @@ def load_train_test(batch_size) -> Tuple[torch.utils.data.DataLoader, torch.util
     data = datasets.ImageFolder(IMG_FOLDER, transform=transforms.Compose([transforms.CenterCrop((128, 128)),
                                                                           transforms.ToTensor()]))
     n_data = len(data)
+    BATCHES_PER_EPOCH = int(np.floor(n_data/batch_size))
     indices = list(range(n_data))
     np.random.shuffle(indices)
     indices = indices[:batch_size * BATCHES_PER_EPOCH]
@@ -181,7 +182,16 @@ def run_trainer(epochs=2, batch_size=144, half_depth=5, loss='l2'):
 
 
 def main():
-    run_trainer(epochs=10, batch_size=144, half_depth=5, loss='l2')
+    loss_list = ['l1', 'l2']
+    epochs_list = [10, 20, 40]
+    half_depth_list = [2, 3, 4, 5]
+    batch_size_list = [10, 100, 1000, 10000]
+    for e in epochs_list:
+        for l in loss_list:
+            for b in batch_size_list:
+                for d in half_depth_list:
+                    run_trainer(epochs=e, batch_size=b, half_depth=d, loss=l)
+
 
 
 if __name__ == "__main__":
